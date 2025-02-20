2b2f17fbb37bd71bb03655d504cadb8e72b346b1
ZionAmar <zion0549774827@hotmail.com>
2025-02-20 20:33:57 +0200

    Config DB connection

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ const port = 5005;
 app.listen(port,()=>{
     console.log(`running on http://localhost:${port}`);    
 })
+//-----------------------------------------//
+const users = require('./routs/users');
+app.use('/users',users);
